03b776de2be9918ebf6c09feba0d6679c04295b2
Tukale <ridwan.tukale@outlook.com>
2020-06-08 17:15:11 +0300

    build

---
+++ .cache/sync-requires.js
@@ -0,0 +1,16 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/air/Desktop/proj/tuke/tukale/.cache/caches/gatsby-plugin-offline/app-shell.js"))),
+  "component---src-pages-404-tsx": hot(preferDefault(require("/Users/air/Desktop/proj/tuke/tukale/src/pages/404.tsx"))),
+  "component---src-pages-blog-tsx": hot(preferDefault(require("/Users/air/Desktop/proj/tuke/tukale/src/pages/blog.tsx"))),
+  "component---src-pages-contact-tsx": hot(preferDefault(require("/Users/air/Desktop/proj/tuke/tukale/src/pages/contact.tsx"))),
+  "component---src-pages-index-tsx": hot(preferDefault(require("/Users/air/Desktop/proj/tuke/tukale/src/pages/index.tsx"))),
+  "component---src-pages-resume-tsx": hot(preferDefault(require("/Users/air/Desktop/proj/tuke/tukale/src/pages/resume.tsx"))),
+  "component---src-templates-blog-post-index-js": hot(preferDefault(require("/Users/air/Desktop/proj/tuke/tukale/src/templates/BlogPost/index.js")))
+}
+
